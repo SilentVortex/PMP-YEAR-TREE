f7e2b8ea8e02a9d5dc4bf07bc42c3846e0780568
SilentVortex <149170463+SilentVortex@users.noreply.github.com>
2025-12-17 15:52:53 +0000

    enemy movement

---
+++ Assets/Scripts/Enemy/Soldier/EnemySoldier.cs
@@ -0,0 +1,40 @@
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class EnemySoldier : MonoBehaviour
+{
+    public float speed; // Enemy movement speed
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    // Update is called once per frame
+    void Move()
+    {   
+        // Move the enemy horizontally
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+    void shoot()
+    {
+        //shoot logic here
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            speed = -speed; // Reverse direction upon hitting a wall
+            //Vector3 scale = transform.localScale;
+            //scale.x *= -1; // Flip the enemy sprite
+            //transform.localScale = scale;
+        }
+    }
+}
