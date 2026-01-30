59e38368753b5d0ccf4bed9aaa9bce7385aeb337
SilentVortex <149170463+SilentVortex@users.noreply.github.com>
2026-01-30 14:19:47 +0000

    Update DeleteOnWallHit.cs

--- Assets/Scripts/Enemy/Projectiles/DeleteOnWallHit.cs
+++ Assets/Scripts/Enemy/Projectiles/DeleteOnWallHit.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 public class DeleteOnWallHit : MonoBehaviour
 {
     public float bulletSpeed;
-    void update()
+    void Update()
     {
         // Move the projectile forward based on its speed
-        transform.Translate(Vector2 * bulletSpeed * Time.deltaTime);
+        transform.Translate(Vector2.right * bulletSpeed * Time.deltaTime);
     }
     // Called when this collider/rigidbody starts colliding with another collider/rigidbody
     void OnCollisionEnter2D(Collision2D collision)
